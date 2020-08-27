e12a5c0986a83411fa5b36d8ed59e47693f1f985
Briik <bryce.lively@gmail.com>
2020-08-27 16:32:22 -0400

    update from repo :/

--- test/election.js
+++ test/election.js
@@ -10,7 +10,7 @@ contract("Election", (accounts) => {
   
   it("initializes candidates with correct values", () => {
     var electionInstance;
-    Election.deployed()
+    return Election.deployed()
     .then((instance) => {
       electionInstance = instance;
       return electionInstance.candidates(1);
@@ -18,14 +18,78 @@ contract("Election", (accounts) => {
     .then((candidate) => {
       assert.equal(candidate.id.toNumber(), 1);
       assert.equal(candidate.name, "Candidate 1");
-      assert.equal(candidate.votes.toNumber(), 0);
+      assert.equal(candidate.voteCount.toNumber(), 0);
       return electionInstance.candidates(2);
     })
     .then((candidate) => {
       assert.equal(candidate.id.toNumber(), 2);
       assert.equal(candidate.name, "Candidate 2");
-      assert.equal(candidate.votes.toNumber(), 0);
+      assert.equal(candidate.voteCount.toNumber(), 0);
       return electionInstance;
     })
+  });
+
+  it("allows a voter to cast a vote", () => {
+    var candidateId;
+    return Election.deployed().then((instance) => {
+      electionInstance = instance;
+      candidateId = 1;
+      return electionInstance.vote(candidateId, { from: accounts[0] });
+    }).then((receipt) => {
+      assert.equal(receipt.logs.length, 1, "an event was triggered");
+      assert.equal(receipt.logs[0].event, "votedEvent", "the event type is correct");
+      assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
+      return electionInstance.voters(accounts[0]);
+    }).then((voted) => {
+      assert(voted, "the voter was marked as voted");
+      return electionInstance.candidates(candidateId);
+    }).then((candidate) => {
+      var voteCount = candidate[2];
+      assert.equal(voteCount, 1, "increments the candidate's vote count");
+    })
+  });
+
+  it("throws an exception for invalid candiates", () => {
+    var electionInstance;
+    return Election.deployed().then((instance) => {
+      electionInstance = instance;
+      return electionInstance.vote(99, { from: accounts[1] })
+    }).then(assert.fail).catch((error) => {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      return electionInstance.candidates(1);
+    }).then((candidate1) => {
+      var voteCount = candidate1[2];
+      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+      return electionInstance.candidates(2);
+    }).then((candidate2) => {
+      var voteCount = candidate2[2];
+      assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
+    });
+  });
+
+  it("throws an exception for double voting", () => {
+    var electionInstance;
+    var candidateId;
+    return Election.deployed().then((instance) => {
+      electionInstance = instance;
+      candidateId = 2;
+      electionInstance.vote(candidateId, { from: accounts[1] });
+      return electionInstance.candidates(candidateId);
+    }).then((candidate) => {
+      var voteCount = candidate[2];
+      assert.equal(voteCount, 1, "accepts first vote");
+      // Try to vote again
+      return electionInstance.vote(candidateId, { from: accounts[1] });
+    }).then(assert.fail).catch((error) => {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      return electionInstance.candidates(1);
+    }).then((candidate1) => {
+      var voteCount = candidate1[2];
+      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+      return electionInstance.candidates(2);
+    }).then((candidate2) => {
+      var voteCount = candidate2[2];
+      assert.equal(voteCount, 1, "candidate 2 did not receive any votes");
+    });
   });
 });
